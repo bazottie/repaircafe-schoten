544f9db75c481939aeea13eadf65367669f8a083
Bas <bas@voorhoede.nl>
2025-09-24 17:37:00 +0200

    Update quote photo

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 export function Quote({ name, quote }: { name: 'mevrouw', quote: string }) {
 	return (
-		<div className="flex gap-4 items-center flex-col md:flex-row">
-			<Image
-				className="aspect-square rounded-full not-prose"
-				height={ 150 }
-				width={ 150 }
-				alt={ name }
-				src={ mevrouw }
-				loading="lazy"
-			/>
-			<blockquote className="border-orange-500">&quot;{ quote }&quot;</blockquote>
+		<div className="flex gap-4  min-[520]:items-center flex-col-reverse min-[520]:flex-row">
+				<Image
+					className="aspect-square rounded-full m-0 not-prose"
+					height={ 100 }
+					width={ 100 }
+					alt={ name }
+					src={ mevrouw }
+					loading="lazy"
+				/>
+			<blockquote className="max-[520px]:mb-0 border-orange-500">&quot;{ quote }&quot;</blockquote>
 		</div>
 	)
 }
